c56ce11040a92d78cccfe7c38355ae357e5f5c20
Julio Anderson <kbjass2@gmail.com>
2020-08-31 23:25:04 -0300

    Finalizing the module

--- Front-end com ReactJS/src/App.js
+++ Front-end com ReactJS/src/App.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
 
+import './App.css';
 import Header from './components/Header';
 
 export default function App () {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+  const [projects, setProjects] = useState([])
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`])
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, [])
+
+  async function handleAddProject() {
+    //setProjects([...projects, `Novo projeto ${Date.now()}`])
+
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Anderson Santana"
+    })
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+
   }
 
   return (
   <>
     <Header title= "Projects" />
 
+
     <ul>
-      {projects.map(project => <li key={project}> {project} </li>)}
+      {projects.map(project => <li key={project.id}> {project.title} </li>)}
     </ul>
 
     <button type='button' onClick={handleAddProject}>Adicionar Projeto</button>
--- Front-end com ReactJS/webpack.config.js
+++ Front-end com ReactJS/webpack.config.js
@@ -16,8 +16,20 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
-      }
+        }},
+        {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},          
+        ],
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader',
+        }},
     ]
   }
 }
